1ba3a5ed0c38a58b9ea8ebe690efad24ea0ffb1e
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-13 17:59:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import link
 import streamlit as st
 col1,col2=st.beta_columns(2)
 with col1:
@@ -5,6 +6,6 @@ with col1:
     st.subheader("Prevención de cancer según edad")
 with col2:
     st.image("WAPP.png",None,350)
-    link="<phttps://elmedpost.wordpress.com</p>"
+    link = '<p style="font-family:Times; color:; font-size: 30px;">SOFA</p>'
     st.markdown(link,unsafe_allow_html=True)
 edad=st.slider("Edad",1,130,None)
