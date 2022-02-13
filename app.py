8abd72afcf982977534d37ae8bdb1a6625ac3711
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-13 17:51:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 col1,col2=st.beta_columns(2)
 with col1:
-    st.title("App para prevención de cancer")
+    st.title("CanPrev App ")
+    st.subheader("Prevención de cancer según edad")
 with col2:
     st.image("WAPP.png",None,350)
 edad=st.slider("Edad",1,130,None)
