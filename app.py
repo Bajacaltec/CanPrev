ef6f60a98140679622e1ef7b318fed2e4b265bcc
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-13 17:50:27 -0600

    act

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
-st.title("App para prevención de cancer")
+col1,col2=st.beta_columns(2)
+with col1:
+    st.title("App para prevención de cancer")
+with col2:
+    st.image("WAPP.png",None,350)
 edad=st.slider("Edad",1,130,None)
