c0899dd13efaeb6f60b2eecfb4762695ac292ba2
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-13 18:02:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,5 @@ with col1:
     st.subheader("Prevención de cancer según edad")
 with col2:
     st.image("WAPP.png",None,350)
-    link = '<link style="font-family:Times; color:; font-size: 30px;">https://elmedpost.wordpress.com</link>'
-    st.markdown(link,unsafe_allow_html=True)
+    st.subheader(https://elmedpost.wordpress.com)
 edad=st.slider("Edad",1,130,None)
